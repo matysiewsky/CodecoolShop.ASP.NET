3a6ee2a32dade2cda3e868b5421a4cc1bedea439
Maciej M <matysiewskimaciej@gmail.com>
2022-01-20 22:47:09 +0100

    using refactor in Supplier Service tests

--- src/Codecool.Shop.Test/SupplierServiceTest/SupplierServiceShould.cs
+++ src/Codecool.Shop.Test/SupplierServiceTest/SupplierServiceShould.cs
@@ -1,7 +1,5 @@
 using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Linq.Expressions;
 using Codecool.Shop.ASP.NET.Service.Interfaces;
 using Codecool.Shop.ASP.NET.Service.Services;
 using Codecool.Shop.Domain.Models;
